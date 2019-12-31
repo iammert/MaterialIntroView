1a703373569a719b00b79482b737908e80f08992
邓超@xgtl <2325690622@qq.com>
2019-12-31 15:53:39 +0800

    Migrate to androidx

--- sample/src/main/java/co/mobiwise/sample/MainActivity.java
+++ sample/src/main/java/co/mobiwise/sample/MainActivity.java
@@ -2,12 +2,12 @@ package co.mobiwise.sample;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import com.google.android.material.navigation.NavigationView;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
